4aaefdaf90601eacea1bd381ab12de00a16afa3c
Jonno Schmidt <jonno.schmidt@duke.edu>
2012-11-29 02:45:44 -0500

    Hello

--- web.js
+++ web.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var mongo = require('mongodb');
 
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(" mongodb://norboglirbane:norboglir@ds033757.mongolab.com:33757/heroku_app9592075", 
+	function (err, db)
+	{
+		if (!err)
+			console.log('connected');
+	}
+);
+
 var app = express.createServer(express.logger());
 app.configure( function() {
 
